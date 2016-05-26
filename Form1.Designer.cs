99fd89884deb41f4f1c175f7b67ddddcbb96d230
wangyin2111 <534386720@qq.com>
2016-05-26 16:37:06 +0800

    初次建立版本
    建立版本

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace open_bin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openfilebutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开test.bin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(219, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(457, 249);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // openfilebutton
+            // 
+            this.openfilebutton.Location = new System.Drawing.Point(37, 86);
+            this.openfilebutton.Name = "openfilebutton";
+            this.openfilebutton.Size = new System.Drawing.Size(105, 23);
+            this.openfilebutton.TabIndex = 2;
+            this.openfilebutton.Text = "打开文件";
+            this.openfilebutton.UseVisualStyleBackColor = true;
+            this.openfilebutton.Click += new System.EventHandler(this.openfilebutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 307);
+            this.Controls.Add(this.openfilebutton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Button openfilebutton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace open_bin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Mytext = "";
+            int file_len;
+            int read_len;
+            byte[] binchar = new byte[] { };
+
+            FileStream Myfile = new FileStream("test.bin", FileMode.Open, FileAccess.Read);
+            BinaryReader binreader = new BinaryReader(Myfile);
+
+            file_len = (int)Myfile.Length;//获取bin文件长度
+
+            while (file_len > 0)
+            {
+                if (file_len / 256 > 0)//一次读取256字节
+                    read_len = 256;
+                else                   //不足256字节按实际长度读取
+                    read_len = file_len % 256;
+
+                binchar = binreader.ReadBytes(read_len);
+
+                foreach (byte j in binchar)
+                {
+                    Mytext += j.ToString("X2");
+                    Mytext += " ";
+                }
+
+                file_len -= read_len;
+             }
+            textBox1.Text = Mytext;
+            binreader.Close();
+        }
+
+        private void openfilebutton_Click(object sender, EventArgs e)
+        {
+            string Mytext = "";
+            int file_len = 1024;
+            int read_len;
+            Stream myStream = null;
+            byte[] binchar = new byte[] { };
+            BinaryReader binreader;
+
+            OpenFileDialog file1 = new OpenFileDialog();
+            file1.Filter = "bin|*.bin";
+
+            if(file1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = file1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            while (file_len > 0)
+                            {
+                                if (file_len / 256 > 0)//一次读取256字节
+                                    read_len = 256;
+                                else                   //不足256字节按实际长度读取
+                                    read_len = file_len % 256;
+
+                                //binchar = binreader.ReadBytes(read_len);
+
+                                foreach (byte j in binchar)
+                                {
+                                    Mytext += j.ToString("X2");
+                                    Mytext += " ";
+                                }
+
+                                file_len -= read_len;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
